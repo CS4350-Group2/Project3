45f76cece9737a0a4110d4345a630668bbe77ddb
xglenng <xglenng@gmail.com>
2015-03-30 20:02:58 -0600

    initial commit

--- public/index.php
+++ public/index.php
@@ -1,8 +1,19 @@
 <?php
 require '../vendor/autoload.php';
 
+
 $app = new \Slim\Slim();
+
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
+
+$app->post('/', function () {
+    echo "I am Garrett";
+});
+
+$app->post('/auth', function () {
+    echo "instantiate controller here";
+});
+
 $app->run();
