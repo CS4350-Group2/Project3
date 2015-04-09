3d5261f9e8f26f8daabca75a6f25511c92a83b02
xglenng <xglenng@gmail.com>
2015-04-08 19:28:05 -0600

    bootstrapped login form

--- src/Views/LoginForm.php
+++ src/Views/LoginForm.php
@@ -3,16 +3,37 @@
     <head>
         <meta charset="UTF-8">
         <title>Example Login Form</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     </head>
+
     <body>
     
-        <div align="center" id="loginForm">
+        <!-- <div align="center" id="loginForm">
             <p>
                 Username: <input type="text" name="username" size="15" /><br />
                 Password: <input type="password" name="password" size="15" /><br />
                 <p><input type="submit" value="Login" onclick="formSubmit"/></p>
            </p>
-        </div> 
+        </div>  -->
+
+        <form class="form-inline" id="loginForm">
+          <div class="form-group">
+            <label for="exampleInputName2">Username: </label>
+            <input type="text" class="form-control" id="username" placeholder="username">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail2">Password: </label>
+            <input type="email" class="form-control" id="password" placeholder="password">
+          </div>
+          <button type="submit" class="btn btn-default" onclick="formSubmit">Submit</button>
+        </form>
 
 </form>
 </body>
