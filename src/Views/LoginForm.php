d4f37dae244aa826d0586be15028b177ceac7519
ArieStewart <arielstewart37@gmail.com>
2015-04-08 21:42:34 -0600

    Fix for autoloader

--- src/Views/LoginForm.php
+++ src/Views/LoginForm.php
@@ -10,47 +10,61 @@
             <p>
                 Username: <input type="text" name="username" size="15" /><br />
                 Password: <input type="password" name="password" size="15" /><br />
-                <p><input type="submit" value="Login" onclick="formSubmit"/></p>
+                <p><input type="submit" value="Login" onclick="formSubmit()"/></p>
            </p>
         </div> 
 
 </form>
+
 </body>
-</html>
+
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
 
 <script>
+
+    $(document).ready(function() {
+    });
+
     function formSubmit() {
-        $.post('/login/', $("#loginForm").serialize()).done(function (data) {
-        
-          alert(data);
+        var jsonRequestBody = $("loginForm").serialize();
+        var responseData = $.post("/api", jsonRequestBody)
+            .done(function( data ) {
+                console.log( "Data Loaded: " + data );
+                // Handle login success
+                location.href = "/welcome";
+            });
+
+        responseData.fail(function() {
+            // Handle login failure
+            location.href = "/register";
         });
-        $.ajax({
-            type:"POST",
-            url:"/api",
-            data:data,
-            cache: false,
-            dataType: 'JSON',
-            statusCode:
-            {
-                200: function()
-                {
-                    alert('success');
-                    location.href= "/profile"
-                },
-                401: function()
-                {
-                    alert('unauthorized user');
-                    location.href="/register", name, password
-                }
-                404: function() 
-                {
-                    alert( "page not found" );
-                }
-            }
-        });
+//        $.ajax({
+//            type:"POST",
+//            url:"/api",
+//            data:$("loginForm").serialize(),
+//            cache: false,
+//            dataType: 'JSON',
+//            statusCode:
+//            {
+//                200: function()
+//                {
+//                    alert('success');
+//                    location.href= "/welcome"
+//                },
+//                401: function()
+//                {
+//                    alert('unauthorized user');
+//                    location.href="/register"
+//                }
+//                //404: function()
+//                //{
+//                  //  alert( "page not found" );
+//                //}
+//            }
+//        });
     }
 </script>
 
+</html>
